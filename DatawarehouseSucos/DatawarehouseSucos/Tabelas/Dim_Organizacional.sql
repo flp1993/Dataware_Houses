5ff1b9358917bc863576d52315547a161bcce67b
Felipe Albuquerque <luisfeliperodriguesalbuquerque@yahoo.com.br>
2020-09-16 12:39:38 -0300

    Dataware House Sucos Completo

---
+++ DatawarehouseSucos/DatawarehouseSucos/Tabelas/Dim_Organizacional.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Dim_Organizacional]
+(
+	[Cod_Filho] NVARCHAR(50) NOT NULL PRIMARY KEY,
+	[Desc_Filho] NVARCHAR(200) NULL,
+	[Cod_Pai] NVARCHAR(50) NULL,
+	[Esquerda] int NULL,
+	[Direita] int NULL,
+	[Nivel] int NULL, 
+    CONSTRAINT [FK_Dim_Organizacional_Dim_Organizacional] FOREIGN KEY ([Cod_Pai]) REFERENCES [Dim_Organizacional]([Cod_Filho])
+)
